2e991ed14c4e03357e0bdc5ed1937e468b49f615
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-10 23:18:58 +0530

    COUNT CHARACTER PRESENT IN THE STRING

---
+++ P10.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.*;
+public class P10 {
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter the string :");
+		String s=scan.nextLine();
+		int count=0;
+		for (int i = 0; i < s.length(); i++) {
+			count++;
+		}
+		System.out.println("The number of characters prasent in the string :"+count);
+	}
+}
