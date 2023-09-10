7badf04b51c64b5b644ecbfe0f3d6e53377b171c
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-10 23:14:33 +0530

    CONVERT LOWERCASE VALUE INTO UPPERCASE

---
+++ P5.java
@@ -0,0 +1,11 @@
+package Strings;
+import java.util.*;
+public class P5 {
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter the string :");
+		String s=scan.nextLine();
+		System.out.println(s.toUpperCase());
+		
+	}
+}
