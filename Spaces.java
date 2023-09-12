c972c87c9993c34722c21f43bafa5f631dc99a6b
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-12 15:16:15 +0530

    Remove Spaces

---
+++ Spaces.java
@@ -0,0 +1,13 @@
+package practice;
+import java.util.*;
+public class Spaces {
+	static Scanner scan= new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter the string :");
+		String s=scan.nextLine();
+		String[] s1=s.split(" ");
+		for (String string : s1) {
+			System.out.print(string);
+		}
+	}
+}
