57d232de5b88769155b8eff7f7ea370f6a9369c4
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-13 23:53:34 +0530

    STRING INTO INTEGER

---
+++ P20.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.*;
+public class P20 {
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) {
+	    System.out.print("enter the string :");
+	    String s1=scan.nextLine();
+	    
+	    for(int i=0;i<s1.length();i++) {
+	    	System.out.print(s1.charAt(i)+0);
+	    }
+	}
+
+}
